1f19678681c62f101ad6f06f360bacf679acf15b
Brijit Adak <brijitadak000@gmail.com>
2025-12-02 17:34:09 +0530

    add

--- src/main/java/com/example/journalApp/controller/HealthCheck.java
+++ src/main/java/com/example/journalApp/controller/HealthCheck.java
@@ -1,12 +1,12 @@
 package com.example.journalApp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-public class HealthCheck{
+public class HealthCheck {
     @GetMapping("/health-check")
-    public String healthCheck(){
+    public String healthCheck() {
         return "OK";
     }
 }
