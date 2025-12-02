9dad7fc4900fdd5eeee32f70fb517246ad27eee9
Brijit Adak <brijitadak000@gmail.com>
2025-12-02 09:53:00 +0530

    add

--- src/main/java/com/example/journalApp/JournalApplication.java
+++ src/main/java/com/example/journalApp/JournalApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JournalAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JournalAppApplication.class, args);
-	}
-
+public class JournalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JournalApplication.class, args);
+    }
 }
