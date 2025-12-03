d4b5ae2391ff83580bd58f83b58cc066acdb530e
Brijit Adak <brijitadak000@gmail.com>
2025-12-03 14:47:50 +0530

    add

--- src/main/java/com/example/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -1,8 +1,26 @@
 package com.example.journalApp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.example.journalApp.entity.JournalEntry;
+import java.util.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@RequestMapping("/journal")
 class JournalEntryController {
-
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    } 
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry entry) {
+        journalEntries.put(entry.getId(), entry);
+        return true;
+    } 
 }
